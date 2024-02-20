00ef872a04c208459453cca5365d14931427b1fd
Karolina Zaiats <karolina.ivanitska@gmail.com>
2024-02-20 18:23:08 +0100

    Summary

--- js/task-2.js
+++ js/task-2.js
@@ -1,61 +1,25 @@
-"use strict"
+class Storage{
+  #items;
+  constructor(array) {
+    this.#items = array;
+  }
 
-const getUsersWithFriend = (users, friendName) => users.filter(users => users.friends.includes(friendName));
+  getItems() {
+    return this.#items;
+  }
 
-const allUsers = [
-    {
-      name: "Moore Hensley",
-      friends: ["Sharron Pace"]
-    },
-    {
-      name: "Sharlene Bush",
-      friends: ["Briana Decker", "Sharron Pace"]
-    },
-    {
-      name: "Ross Vazquez",
-      friends: ["Marilyn Mcintosh", "Padilla Garrison", "Naomi Buckner"]
-    },
-    {
-      name: "Elma Head",
-      friends: ["Goldie Gentry", "Aisha Tran"]
-    },
-    {
-      name: "Carey Barr",
-      friends: ["Jordan Sampson", "Eddie Strong"]
-    },
-    {
-      name: "Blackburn Dotson",
-      friends: ["Jacklyn Lucas", "Linda Chapman"]
-    },
-    {
-      name: "Sheree Anthony",
-      friends: ["Goldie Gentry", "Briana Decker"]
-    }
-  ];
-  
-  console.log(getUsersWithFriend(allUsers, "Briana Decker")); 
-  // [
-  //   {
-  //     name: "Sharlene Bush",
-  //     friends: ["Briana Decker", "Sharron Pace"]
-  //   },
-  //   {
-  //     name: "Sheree Anthony",
-  //     friends: ["Goldie Gentry", "Briana Decker"]
-  //   }
-  // ]
-  
-  console.log(getUsersWithFriend(allUsers, "Goldie Gentry"));
-  // [
-  //   {
-  //     name: "Elma Head",
-  //     friends: ["Goldie Gentry", "Aisha Tran"]
-  //   },
-  //   {
-  //     name: "Sheree Anthony",
-  //     friends: ["Goldie Gentry", "Briana Decker"]
-  //   }
-  // ]
-  
-  console.log(getUsersWithFriend(allUsers, "Adrian Cross" )); // []
+  addItem(newItem) {
+    this.#items.push(newItem);
+  }
 
+  removeItem(itemToRemove){
+    this.#items = this.#items.filter(el => el !== itemToRemove);
+  }
+}
+
+const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
+storage.addItem("Droid");
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
+storage.removeItem("Prolonger");
+console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
